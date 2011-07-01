1d1c0dbef98182d7e96b666505a5a6c59ec4d133
marano <thiagomarano@gmail.com>
2011-07-01 18:26:13 -0300

    New leiningen project.

---
+++ src/blockduck/core.clj
@@ -0,0 +1,1 @@
+(ns blockduck.core)
---
+++ test/blockduck/test/core.clj
@@ -0,0 +1,6 @@
+(ns blockduck.test.core
+  (:use [blockduck.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
