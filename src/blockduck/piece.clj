f9779ad1795a5c4ef50d61a79a527cbc293f183a
marano <thiagomarano@gmail.com>
2011-07-04 22:44:32 -0300

    Tell possible corners for domino.

--- src/blockduck/piece.clj
+++ src/blockduck/piece.clj
@@ -6,6 +6,16 @@
    {:x (- (:x location) 1) :y (+ (:y location) 1)}
    {:x (+ (:x location) 1) :y (+ (:y location) 1)}])
 
+(defn possible-corners-for-domino [central-piece other-pieces]
+  (concat (corners-for-monimo central-piece)
+          (mapcat corners-for-monimo other-pieces)))
+
+
+;(defn corners-for-domino [central-piece other-pieces]
+;  (let possible-corners
+;    (concat [(corners-for-monimo central-piece)]
+;            (mapcat corners-for-monimo other-pieces))))
+
 (defn monimo-at [location]
   (let [x (:x location)
         y (:y location)]
--- test/blockduck/test/piece.clj
+++ test/blockduck/test/piece.clj
@@ -4,3 +4,6 @@
 
 (deftest shouldTellCornersForAMonimo 
   (is (= [{:x 0 :y 0} {:x 2 :y 0} {:x 0 :y 2} {:x 2 :y 2}] (corners-for-monimo {:x 1 :y 1}))))
+
+(deftest shouldTellPossibleCornersForADomino
+  (is (= [{:x 0 :y 0} {:x 2 :y 0} {:x 0 :y 2} {:x 2 :y 2} {:x 0 :y 1} {:x 2 :y 1} {:x 0 :y 3} {:x 2 :y 3}] (possible-corners-for-domino {:x 1 :y 1} [{:x 1 :y 2}]))))
