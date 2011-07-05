19f40ef55a48251eb9d782bfaa699f68fa5bfa9b
marano <thiagomarano@gmail.com>
2011-07-05 01:29:06 -0300

    Renames impossible spot to blocked.

--- src/blockduck/piece.clj
+++ src/blockduck/piece.clj
@@ -6,28 +6,28 @@
    {:x (- (:x location) 1) :y (+ (:y location) 1)}
    {:x (+ (:x location) 1) :y (+ (:y location) 1)}])
 
-(defn impossible-corners-for-monimo [location]
+(defn corners-blocked-by-monimo [location]
   [{:x (:x location) :y (- (:y location) 1)}
    {:x (:x location) :y (+ (:y location) 1)}
    {:x (- (:x location) 1) :y (:y location)}
    {:x (+ (:x location) 1) :y (:y location)}])
 
-(defn impossible-corners-for-domino [central-piece other-pieces]
-  (concat (impossible-corners-for-monimo central-piece)
-          (mapcat impossible-corners-for-monimo other-pieces)))
+(defn corners-blocked-by-domino [central-piece other-pieces]
+  (concat (corners-blocked-by-monimo central-piece)
+          (mapcat corners-blocked-by-monimo other-pieces)))
 
-(defn possible-corners-for-domino [central-piece other-pieces]
+(defn corners-for-domino [central-piece other-pieces]
   (concat (corners-for-monimo central-piece)
           (mapcat corners-for-monimo other-pieces)))
 
-(defn corners-for-domino [central-piece other-pieces]
-  (let [impossible-spots (impossible-corners-for-domino central-piece other-pieces)]
+(defn available-corners-for-domino [central-piece other-pieces]
+  (let [blocked-spots (corners-blocked-by-domino central-piece other-pieces)]
     (filter
       (complement
         (fn [possible-monimo]
           (some (fn [impossible-monimo]
-                  (= possible-monimo impossible-monimo)) impossible-spots)))
-      (possible-corners-for-domino central-piece other-pieces))))
+                  (= possible-monimo impossible-monimo)) blocked-spots)))
+      (corners-for-domino central-piece other-pieces))))
 
 (defn monimo-at [location]
   (let [x (:x location)
--- test/blockduck/test/piece.clj
+++ test/blockduck/test/piece.clj
@@ -5,14 +5,14 @@
 (deftest shouldTellCornersForAMonimo 
   (is (= [{:x 0 :y 0} {:x 2 :y 0} {:x 0 :y 2} {:x 2 :y 2}] (corners-for-monimo {:x 1 :y 1}))))
 
-(deftest shouldTellPossibleCornersForADomino
-  (is (= [{:x 0 :y 0} {:x 2 :y 0} {:x 0 :y 2} {:x 2 :y 2} {:x 0 :y 1} {:x 2 :y 1} {:x 0 :y 3} {:x 2 :y 3}] (possible-corners-for-domino {:x 1 :y 1} [{:x 1 :y 2}]))))
-
-(deftest shouldTellImpossibleCornersForAMomino
-  (is (= [{:x 1 :y 0} {:x 1 :y 2} {:x 0 :y 1} {:x 2 :y 1}] (impossible-corners-for-monimo {:x 1 :y 1}))))
-
-(deftest shouldTellImpossibleCornersForADomino
-  (is (= [{:x 1 :y 0} {:x 1 :y 2} {:x 0 :y 1} {:x 2 :y 1} {:x 1 :y 1} {:x 1 :y 3} {:x 0 :y 2} {:x 2 :y 2}] (impossible-corners-for-domino {:x 1 :y 1} [{:x 1 :y 2}]))))
-
 (deftest shouldTellCornersForADomino
-  (is (= [{:x 0 :y 0} {:x 2 :y 0} {:x 0 :y 3} {:x 2 :y 3}] (corners-for-domino {:x 1 :y 1} [{:x 1 :y 2}]))))
+  (is (= [{:x 0 :y 0} {:x 2 :y 0} {:x 0 :y 2} {:x 2 :y 2} {:x 0 :y 1} {:x 2 :y 1} {:x 0 :y 3} {:x 2 :y 3}] (corners-for-domino {:x 1 :y 1} [{:x 1 :y 2}]))))
+
+(deftest shouldTellCornersBlockedByAMonimo
+  (is (= [{:x 1 :y 0} {:x 1 :y 2} {:x 0 :y 1} {:x 2 :y 1}] (corners-blocked-by-monimo {:x 1 :y 1}))))
+
+(deftest shouldTellCornersBlockedByADomino
+  (is (= [{:x 1 :y 0} {:x 1 :y 2} {:x 0 :y 1} {:x 2 :y 1} {:x 1 :y 1} {:x 1 :y 3} {:x 0 :y 2} {:x 2 :y 2}] (corners-blocked-by-domino {:x 1 :y 1} [{:x 1 :y 2}]))))
+
+(deftest shouldTellAvailableCornersForADomino
+  (is (= [{:x 0 :y 0} {:x 2 :y 0} {:x 0 :y 3} {:x 2 :y 3}] (available-corners-for-domino {:x 1 :y 1} [{:x 1 :y 2}]))))
