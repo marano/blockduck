012ce0deb3a9cbef6de0195b5af2871a8625ffc2
marano <thiagomarano@gmail.com>
2011-07-15 00:12:14 -0300

    Piece is the future.

--- test/blockduck/test/piece.clj
+++ test/blockduck/test/piece.clj
@@ -1,47 +1,18 @@
 (ns blockduck.test.piece
   (:use [blockduck.piece])
+  (:use [blockduck.point])
   (:use [midje.sweet]))
 
-(fact "tells corners for a monomino"
-  (corners-for-monomino {:x 1 :y 1}) => [{:x 0 :y 0} {:x 2 :y 0} {:x 0 :y 2} {:x 2 :y 2}])
+(fact "tells piece board points"
+  (let [reference-point (point 1 1)
+        blocks (points 0 0, 0 1)
+        piece (piece reference-point blocks)]
+    (piece-board-points piece) => (points 1 1, 1 2)))
 
-(fact "tells corners for a domino"
-  (corners-for-piece {:x 1 :y 1} [{:x 0 :y 1}]) => [{:x 0 :y 0} {:x 2 :y 0} {:x 0 :y 2} {:x 2 :y 2} {:x 0 :y 1} {:x 2 :y 1} {:x 0 :y 3} {:x 2 :y 3}])
+(fact "tells points touched by a triomino and do not duplicate overlaping points"
+  (let [a-piece (piece (point 1 1) (points 0 0, 0 1, 0 2))]
+    (points-touched-by-piece a-piece) => (points 0 0, 2 0, 0 2, 2 2, 0 1, 2 1, 0 3, 2 3, 0 4, 2 4)))
 
-(fact "tells corners for a triomino and do not duplicate overlaping corners"
-  (corners-for-piece {:x 1 :y 1} [{:x 0 :y 1} {:x 0 :y 2}]) => [{:x 0 :y 0} {:x 2 :y 0} {:x 0 :y 2} {:x 2 :y 2} {:x 0 :y 1} {:x 2 :y 1} {:x 0 :y 3} {:x 2 :y 3} {:x 0 :y 4} {:x 2 :y 4}])
-
-(fact "tells spots blocked by a monomino"
-  (spots-blocked-by-monomino {:x 1 :y 1}) => [{:x 1 :y 1} {:x 1 :y 0} {:x 1 :y 2} {:x 0 :y 1} {:x 2 :y 1}])
-
-(fact "tells spots blocked by a domino and not duplicate overlaping spots"
-  (spots-blocked-by-piece {:x 1 :y 1} [{:x 0 :y 1}]) => [{:x 1 :y 1} {:x 1 :y 0} {:x 1 :y 2} {:x 0 :y 1} {:x 2 :y 1} {:x 1 :y 3} {:x 0 :y 2} {:x 2 :y 2}])
-
-(fact "tells available corners for a domino"
-  (available-corners-for-piece {:x 1 :y 1} [{:x 0 :y 1}]) => [{:x 0 :y 0} {:x 2 :y 0} {:x 0 :y 3} {:x 2 :y 3}])
-
-(fact "tells available corners for a triomino"
-  (available-corners-for-piece {:x 1 :y 1} [{:x 1 :y 0} {:x 0 :y 1}]) => [{:x 0 :y 0} {:x 3 :y 0} {:x 3 :y 2} {:x 0 :y 3} {:x 2 :y 3}])
-
-(fact "tells absolute location based on relative position"
-  (absolute-location {:x 2 :y 2} {:x 1 :y 0}) => {:x 3 :y 2}
-  (absolute-location {:x 2 :y 2} {:x -1 :y 0}) => {:x 1 :y 2}
-  (absolute-location {:x 2 :y 2} {:x 0 :y 1}) => {:x 2 :y 3}
-  (absolute-location {:x 2 :y 2} {:x 0 :y -1}) => {:x 2 :y 1})
-
-(fact "tells the absolute location of a list of relative locations"
-  (absolute-locations {:x 1 :y 1} [{:x 0 :y 1}]) => [{:x 1 :y 2}])
-
-(fact "tells the new spot for a relative rotated monomino"
-  (relative-rotate-monomino {:x -1 :y 2} 1) => {:x 2 :y 1}
-  (relative-rotate-monomino {:x -1 :y 2} 2) => {:x 1 :y -2}
-  (relative-rotate-monomino {:x -1 :y 2} 3) => {:x -2 :y -1})
-
-(fact "tells new spots for rotated piece"
-  (relative-rotate-pieces [{:x 1 :y 1} {:x 0 :y 1}] 1) => [{:x 1 :y -1} {:x 1 :y 0}])
-
-(fact "tells new spot for relative fliped monomino"
-  (relative-flip-monomino {:x -1 :y 1}) => {:x 1 :y 1})
-
-(fact "tells new spots for a relative fliped piece"
-  (relative-flip-pieces [{:x -1 :y 0} {:x -1 :y 1} {:x 1 :y 0} {:x 1 :y -1}]) => [{:x 1 :y 0} {:x 1 :y 1} {:x -1 :y 0} {:x -1 :y -1} ])
+(fact "tells points blocked by piece"
+  (let [a-piece (piece (point 1 1) (points 0 0))]
+    (points-blocked-by-piece a-piece) => (points 1 1, 1 0, 1 2, 0 1, 2 1)))
