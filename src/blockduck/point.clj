72d6cc5bdb3d974cba9eddbd0558508e89b8bd80
marano <thiagomarano@gmail.com>
2011-07-15 13:20:47 -0300

    Changed interface for creating points.

--- src/blockduck/point.clj
+++ src/blockduck/point.clj
@@ -2,15 +2,19 @@
 
 (defrecord Point [x y])
 
-(defn point [x y] (Point. x y))
+(defn- point [x y] (Point. x y))
 
-(defn points [& coordinates]
-  (map (fn [coordinates] (apply point coordinates)) (partition 2 coordinates)))
+(def xy point)
+
+(defn- points [& coordinates]
+  (map (fn [coordinates] (apply xy coordinates)) (partition 2 coordinates)))
+
+(def xys points)
 
 (defn board-point [reference-point relative-point]
   (let [x (+ (:x reference-point) (:x relative-point))
         y (+ (:y reference-point) (:y relative-point))]
-    (Point. x y)))
+    (xy x y)))
 
 (defn board-points [reference-point relative-points]
   (map #(board-point reference-point %) relative-points))
@@ -18,16 +22,16 @@
 (defn points-touched-by-point [a-point]
   (let [x (:x a-point)
         y (:y a-point)]
-    (points (- x 1) (- y 1)
-            (+ x 1) (- y 1)
-            (- x 1) (+ y 1)
-            (+ x 1) (+ y 1))))
+    (xys (- x 1) (- y 1)
+         (+ x 1) (- y 1)
+         (- x 1) (+ y 1)
+         (+ x 1) (+ y 1))))
 
 (defn points-blocked-by-point [a-point]
   (let [x (:x a-point)
         y (:y a-point)]
-    (points x y
-            x (- y 1)
-            x (+ y 1)
-            (- x 1) y
-            (+ x 1) y)))
+    (xys x y
+         x (- y 1)
+         x (+ y 1)
+         (- x 1) y
+         (+ x 1) y)))
--- test/blockduck/test/piece.clj
+++ test/blockduck/test/piece.clj
@@ -4,15 +4,15 @@
   (:use [midje.sweet]))
 
 (fact "tells piece board points"
-  (let [reference-point (point 1 1)
-        blocks (points 0 0, 0 1)
+  (let [reference-point (xy 1 1)
+        blocks (xys 0 0, 0 1)
         piece (piece reference-point blocks)]
-    (piece-board-points piece) => (points 1 1, 1 2)))
+    (piece-board-points piece) => (xys 1 1, 1 2)))
 
 (fact "tells points touched by a triomino and do not duplicate overlaping points"
-  (let [a-piece (piece (point 1 1) (points 0 0, 0 1, 0 2))]
-    (points-touched-by-piece a-piece) => (points 0 0, 2 0, 0 2, 2 2, 0 1, 2 1, 0 3, 2 3, 0 4, 2 4)))
+  (let [a-piece (piece (xy 1 1) (xys 0 0, 0 1, 0 2))]
+    (points-touched-by-piece a-piece) => (xys 0 0, 2 0, 0 2, 2 2, 0 1, 2 1, 0 3, 2 3, 0 4, 2 4)))
 
 (fact "tells points blocked by piece"
-  (let [a-piece (piece (point 1 1) (points 0 0))]
-    (points-blocked-by-piece a-piece) => (points 1 1, 1 0, 1 2, 0 1, 2 1)))
+  (let [a-piece (piece (xy 1 1) (xys 0 0))]
+    (points-blocked-by-piece a-piece) => (xys 1 1, 1 0, 1 2, 0 1, 2 1)))
