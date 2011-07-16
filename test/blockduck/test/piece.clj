8a20c239d06a67f269e67af594b33ad136a0dc38
marano <thiagomarano@gmail.com>
2011-07-16 11:27:54 -0300

    Proper identation.

--- test/blockduck/test/piece.clj
+++ test/blockduck/test/piece.clj
@@ -4,23 +4,23 @@
   (:use [midje.sweet]))
 
 (fact "tells piece points on the board"
-  (let [reference-point (xy 1 1)
-        blocks (xys 0 0, 0 1)
-        piece (piece reference-point blocks)]
-    (piece-points-on-the-board piece) => (xys 1 1, 1 2)))
+      (let [reference-point (xy 1 1)
+            blocks (xys 0 0, 0 1)
+            piece (piece reference-point blocks)]
+        (piece-points-on-the-board piece) => (xys 1 1, 1 2)))
 
 (fact "tells corners for a given piece"
-  (let [a-piece (piece (xy 1 1) (xys 0 0, 0 1, 0 2))]
-    (piece-corners a-piece) => (xys 0 0, 2 0, 0 2, 2 2, 0 1, 2 1, 0 3, 2 3, 0 4, 2 4)))
+      (let [a-piece (piece (xy 1 1) (xys 0 0, 0 1, 0 2))]
+        (piece-corners a-piece) => (xys 0 0, 2 0, 0 2, 2 2, 0 1, 2 1, 0 3, 2 3, 0 4, 2 4)))
 
 (fact "tells points blocked by a given piece"
-  (let [a-piece (piece (xy 1 1) (xys 0 0, 0 1))]
-    (points-blocked-by-piece a-piece) => (xys 1 1, 1 0, 1 2, 0 1, 2 1, 1 3, 0 2, 2 2 )))
+      (let [a-piece (piece (xy 1 1) (xys 0 0, 0 1))]
+        (points-blocked-by-piece a-piece) => (xys 1 1, 1 0, 1 2, 0 1, 2 1, 1 3, 0 2, 2 2 )))
 
 (fact "flips piece"
-  (flip-piece (piece (xy 1 1) (xys 0 0, 1 0, 0 1))) => (piece (xy 1 1) (xys 0 0, -1 0, 0 1)))
+      (flip-piece (piece (xy 1 1) (xys 0 0, 1 0, 0 1))) => (piece (xy 1 1) (xys 0 0, -1 0, 0 1)))
 
 (fact "rotates piece"
-  (rotate-piece-90 (piece (xy 1 1) (xys 0 0, 0 1))) => (piece (xy 1 1) (xys 0 0, 1 0))
-  (rotate-piece-180 (piece (xy 1 1) (xys 0 0, 0 1))) => (piece (xy 1 1) (xys 0 0, 0 -1))
-  (rotate-piece-270 (piece (xy 1 1) (xys 0 0, 0 1))) => (piece (xy 1 1) (xys 0 0, -1 0)))
+      (rotate-piece-90 (piece (xy 1 1) (xys 0 0, 0 1))) => (piece (xy 1 1) (xys 0 0, 1 0))
+      (rotate-piece-180 (piece (xy 1 1) (xys 0 0, 0 1))) => (piece (xy 1 1) (xys 0 0, 0 -1))
+      (rotate-piece-270 (piece (xy 1 1) (xys 0 0, 0 1))) => (piece (xy 1 1) (xys 0 0, -1 0)))
