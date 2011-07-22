c2e3f6c679816ed2d06684be2468f9f8220c5f5a
marano <thiagomarano@gmail.com>
2011-07-22 15:33:04 -0300

    Description on the project file.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject blockduck "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Block Duck"
   :dependencies [[org.clojure/clojure "1.2.1"]]
   :dev-dependencies [[midje "1.1.1"] [clojure-contrib "1.2.0"]])
